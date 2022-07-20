b0d310861e220a5b1038080a0f5730b538b9eb1f
Michael Sieler <sielerjm@oregonstate.edu>
2022-07-20 11:48:00 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -105,6 +105,14 @@ blog_feed_archives = True
 blog_feed_fulltext = True
 blog_feed_length = 10
 
-# This will detect a blog post no matter where it is
+# This will detect a blog post no matter where it is in /posts or what format
 # https://ablog.readthedocs.io/manual/posting-and-listing/#posting-front-matter
 blog_post_pattern = ["posts/*.rst", "posts/*.md"]
+
+# Custom sidebar templates, maps document names to template names.
+html_sidebars = {
+   '**': [
+          'postcard.html', 'recentposts.html',
+          'tagcloud.html', 'categories.html',
+          'archives.html', ]
+}
